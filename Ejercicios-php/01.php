1651f96cdfcd4d8a0d75cbd8c29d17e17f5793d4
Kv1n-ez <159096492+Kv1n-ez@users.noreply.github.com>
2024-04-01 09:21:10 -0600

    Add files via upload

---
+++ Ejercicios-php/01.php
@@ -0,0 +1,7 @@
+<?php
+$numero1 = 10;
+$numero2 = 5;
+$suma = $numero1 + $numero2;
+
+echo "El resultado de $numero1 + $numero2 es $suma";
+?>
---
+++ Ejercicios-php/Formulario/index.php
@@ -0,0 +1,4 @@
+<form action="saludo.php" method="post">
+    Nombre: <input type="text" name="nombre">
+    <input type="submit">
+</form>
